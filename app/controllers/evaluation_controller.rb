79e209900f019363601947018508a6de6e17d07f
Fujikawa, K <vhintz@rcpt.at>
2019-07-19 10:15:19 +0000

    implement q1-2, 1-3

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -2,7 +2,7 @@
 
 class EvaluationController < ApplicationController
   def result
-    render html: "回答 = #{params[:q1-1]}, #{params[:q2]}"
+    render html: "回答 = #{params[:q1-1]}, #{params[:q1-2]}, #{params[:q1-3]}"
   end
 end
 
