3c610c600aa2d2374bf37f9444e1b97c86bff028
TakeshiHora <a1936th@aiit.ac.jp>
2019-07-21 08:48:13 +0000

    cahnged html

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,5 @@ Rails.application.routes.draw do
   post 'evaluation/result'
   resources :users
   root 'users#new'
-  post "users/create" => "static_pages#check"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
