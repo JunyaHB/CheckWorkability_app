81e63a6c1913939384f41c88fdf8737635bcb1fb
kodama_t <k.tak1014@gmail.com>
2019-07-24 17:24:16 +0900

    db evaluate deta insert

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -3,6 +3,7 @@
 class EvaluationController < ApplicationController
   def result
     render html: "回答 = #{params[:q11]}, #{params[:q12]}, #{params[:q13]}"
+    evaluations = Evaluations.create(score: params[:q11],email: '')
   end
 end
 
