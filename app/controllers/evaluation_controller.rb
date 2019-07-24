3324715a44edf02fb2c6bf87948503957c9decf3
kodama_t <k.tak1014@gmail.com>
2019-07-25 04:14:33 +0900

    db evaluate deta insert fix

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -3,7 +3,7 @@
 class EvaluationController < ApplicationController
   def result
     render html: "回答 = #{params[:q11]}, #{params[:q12]}, #{params[:q13]}, #{params[:q14]}, #{params[:q15]}, #{params[:q16]}"
-    @evaluations = Parameters.all
+    @evaluations = Parameters.new(evaluations_params)
     @evaluations.save
   end
 
