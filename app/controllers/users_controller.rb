c112aeb867963443072950880fadc745b54ddba9
TakeshiHora <a1936th@aiit.ac.jp>
2019-07-24 10:21:07 +0000

    changed contoroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,8 +29,10 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { render:'static_pages/check'}
-        format.json { render:'static_pages/check', status: :created, location: @user }
+        respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: '登録出来ました' }
+        format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
