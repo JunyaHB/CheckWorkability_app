67f40f5216f231e1f7d022cd89313393bb79eb51
kodama_t <k.tak1014@gmail.com>
2019-07-26 03:47:34 +0900

    db evaluate deta insert fix

--- app/models/evaluations.rb
+++ app/models/evaluations.rb
@@ -1,4 +1,4 @@
-class Evaluation < ApplicationRecord
+class Evaluations < ApplicationRecord
     validates :score, presence: true, length: { maximum: 50 }
     validates :email, presence: true, length: { maximum: 255 }
 end
