3dd01565cfcefb5729a7a47506c0c7b6bd4722c7
kodama_t <k.tak1014@gmail.com>
2019-07-26 19:59:17 +0900

    score calc

--- app/controllers/evaluation_controller.rb
+++ app/controllers/evaluation_controller.rb
@@ -3,7 +3,92 @@
 class EvaluationController < ApplicationController
   def result
     # render html: "回答 = #{params[:q11]}, #{params[:q12]}, #{params[:q13]}, #{params[:q14]}, #{params[:q15]}, #{params[:q16]}"
-    @evaluations = Evaluations.create(score: params[:q12], email: "")
+    score1 = 0
+
+    case params[:q11]
+    when 'a' then
+      score1 += 2
+    when 'b' then
+      score1 += 3
+    when "c" then
+      score1 += 4
+    when "d" then
+      score1 += 1
+    else
+      score1 -= 25
+    end
+    
+    case params[:q12]
+    when 'a' then
+      score1 += 2
+    when 'b' then
+      score1 += 4
+    when "c" then
+      score1 += 3
+    when "d" then
+      score1 += 1
+    else
+      score1 -= 25
+    end
+    
+    case params[:q13]
+    when 'a' then
+      score1 += 4
+    when 'b' then
+      score1 += 3
+    when "c" then
+      score1 += 2
+    when "d" then
+      score1 += 1
+    else
+      score1 -= 25
+    end
+    
+    case params[:q14]
+    when 'a' then
+      score1 += 2
+    when 'b' then
+      score1 += 1
+    when "c" then
+      score1 += 3
+    when "d" then
+      score1 += 4
+    else
+      score1 -= 25
+    end
+    
+    case params[:q15]
+    when 'a' then
+      score1 += 2
+    when 'b' then
+      score1 += 4
+    when "c" then
+      score1 += 1
+    when "d" then
+      score1 += 3
+    else
+      score1 -= 25
+    end
+    
+    case params[:q16]
+    when 'a' then
+      score1 += 4
+    when 'b' then
+      score1 += 3
+    when "c" then
+      score1 += 2
+    when "d" then
+      score1 += 1
+    else
+      score1 -= 25
+    end
+
+    @evaluations = Evaluations.create(score: pscore1, email: "")
+  end
+
+
+  def score_calc
+
   end
 
 #  def new
