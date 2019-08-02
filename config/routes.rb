33f00799256f86c66edaceed2644e74a999aaee4
kodama_t <k.tak1014@gmail.com>
2019-08-02 15:10:00 +0900

    resultlist add

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'evaluation/result'
   get 'resultlist/resultlist'
-  get 'management/admin'
   get 'static_pages/contact'
   get 'static_pages/check'
   get 'static_pages/result'
   get 'static_pages/aboutus'
   post 'evaluation/result'
-  post 'resultlist' => 'resultlist#resultlist'
-  post 'admin' => 'management#admin'
   resources :users
   root 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
